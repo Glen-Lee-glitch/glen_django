080722da5211391b9cec94cf4961650dc7a03f60
GlenLee <dlrudrn2088@gmail.com>
2023-05-26 23:19:46 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 
+
 class Post(models.Model):
     objects = None
     title = models.CharField(max_length=30)
     content = models.TextField()
-    
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'
+
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}'
     # pk : 각 레코드에 대한 고유값으로 이해하면 됌.
-    
+
     # author: 추후 작성 예정
